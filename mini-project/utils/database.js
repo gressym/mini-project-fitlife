2c24c5a3e0716f476d68a0961e3dcce37bf82e3c
GressyMahadule <140788167+gressym@users.noreply.github.com>
2024-10-04 23:03:57 +0530

    Add files via upload

---
+++ mini-project/utils/database.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async() => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected){
+        console.log("Mongo is already connected.")
+        return
+    }
+
+    try{
+        await mongoose.connect('mongodb://localhost:27017/fitlifedatabase')
+
+        isConnected = true
+        console.log("connected to mongodb")
+    }
+    catch(e){
+
+        console.log(e)
+    }
+}
